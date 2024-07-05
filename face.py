077cf0991f75c519a61396c83e6ff7e34f73c948
Senkathirselvan <senkathirsmart@gmail.com>
2024-07-05 11:14:03 +0530

    To Solve The Image Without Storing The Local Storage. The Image Directly Processed To The Model

---
+++ face.py
@@ -0,0 +1,37 @@
+# face.py
+
+from huggingface_hub import hf_hub_download
+from ultralytics import YOLO
+from supervision import Detections
+from PIL import Image
+
+class Face:
+    def __init__(self):
+        
+        # Download model
+        self.model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", filename="model.pt")
+        print(f"Model downloaded to: {self.model_path}")
+
+        # Load model
+        self.model = YOLO(self.model_path)
+        print("Model loaded successfully")
+
+    def process_frames(self, frame_names):
+        
+        for img in frame_names:
+            
+            # Load image
+            image_path = img
+            print(f"Attempting to load image: {image_path}")
+            image = Image.open(image_path)
+            print(f"Image loaded successfully: {image_path}")
+
+            # Perform inference
+            output = self.model(image)
+            print("Inference completed")
+
+            # Convert results to Detections
+            results = Detections.from_ultralytics(output[0])
+            print("Detections converted")
+
+            print(results)
